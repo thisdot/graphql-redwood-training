77a062c3389d534822bff83d159ba9154e8398d1
Dustin Goodman <dustin@thisdot.co>
2023-11-02 16:54:35 -0500

    add scalar example

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -6,12 +6,18 @@ import services from 'src/services/**/*.{js,ts}';
 
 import { db } from 'src/lib/db';
 import { logger } from 'src/lib/logger';
+import { NonNegativeIntResolver } from 'src/lib/scalars';
 
 export const handler = createGraphQLHandler({
   loggerConfig: { logger, options: {} },
   directives,
   sdls,
   services,
+  schemaOptions: {
+    resolvers: {
+      NonNegativeInt: NonNegativeIntResolver,
+    },
+  },
   onException: () => {
     // Disconnect from your database with an unhandled exception.
     db.$disconnect();
