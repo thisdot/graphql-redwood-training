cd5c7ec6b2a973010a674dec1d5032d7ebf3a49a
Dustin Goodman <dustin@thisdot.co>
2023-11-06 20:19:30 -0600

    add event card

---
+++ web/src/components/EventCard/EventCard.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+
+import { useQuery } from '@redwoodjs/web';
+
+import { timeTag } from 'src/lib/formatters';
+type EventCardProps = {
+  id: string;
+};
+
+const EventCard: FC<EventCardProps> = ({ id }) => {
+  const { data, loading, error } = useQuery(
+    gql`
+      query FindEventById($id: String!) {
+        event(id: $id) {
+          id
+          name
+          description
+          startAt
+        }
+      }
+    `,
+    {
+      variables: {
+        id,
+      },
+    }
+  );
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  if (error) {
+    return `Error: ${error.message}`;
+  }
+
+  const { event } = data;
+
+  return (
+    <div className="event-card">
+      <h3>{event.name}</h3>
+      <strong>{timeTag(event.startAt)}</strong>
+      <p>{event.description}</p>
+    </div>
+  );
+};
+
+export default EventCard;
